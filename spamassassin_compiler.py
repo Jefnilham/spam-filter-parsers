f0c3e0f06b7d38677cd723c38102af91e20b4502
Jefnilham <39832806+Jefnilham@users.noreply.github.com>
2021-10-19 16:33:01 +0800

    Update spamassassin_compiler.py

--- spamassassin_compiler.py
+++ spamassassin_compiler.py
@@ -3,7 +3,11 @@ while serial_number < 158:
     with open('spamassassin_scores', 'r', encoding = 'utf8') as f:
         with open('spamassassin_compiled_scores', 'w', encoding = 'utf8') as ff:
             for line in f:
+                
+                # find a match from spamassassin_scores file
                 if 'X-Spam-Status' in line:
+                    
+                    # if found, take only the score
                     aa = line[line.index('='):line.index('required')]
                     aa = aa[1:]
                     ff.write('%s: %s\n' %(serial_number, aa))
