441e48e59ea4f2ceea42e3b588032423f3aafe48
Jefnilham <39832806+Jefnilham@users.noreply.github.com>
2021-10-19 16:28:00 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,19 +1,21 @@
 
 import os
 xuid = 1
-
 with open('phishing-2020', 'r', encoding="utf8") as f:
+    
+    # boolean flag whether we want to printing lines or not
+    print_lines = False 
 
-    print_lines = False # Boolean state variable to keep track of whether we want to be printing lines or not
-
-    for line in f:
-        
+    for line in f: 
         with open('headers_phishing-test', 'a', encoding="utf8") as ff:
             xuid_str = str(xuid)
             
+            # find an indicator
             if line.startswith("From jose@monkey.org"):
                 ff.write('S/N: %s\n' % xuid_str)
-                print_lines = True # We have found an @ so we can start printing lines
+                
+                # boolean flag to print
+                print_lines = True 
                 xuid += 1
                 
                     
